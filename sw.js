ddc06a2ffdb93e423e408bdd49e449faec0327a9
sabrosin01 <lenninpareds1@gmail.com>
2026-06-11 01:21:02 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,3 @@
-const CACHE = 'irianis-v2';
-const FILES = ['/Irisapp/', '/Irisapp/index.html', '/Irisapp/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => 
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-});
-
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(fetch(e.request));
 });
